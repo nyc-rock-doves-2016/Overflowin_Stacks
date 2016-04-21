7718777a395adb034c3aabf9b6bca9364091432a
JonSchwartz93 <josch@umich.edu>
2016-04-21 11:23:32 -0400

    Modify Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,25 @@ User.create!(
   email: "123@abc.com"
   )
 
-9.times do User.create!(
+5.times do User.create!(
   username: Faker::Internet.user_name, 
   password: "pw", 
   email: Faker::Internet.email
   )
-
 end
-
-
 
 10.times do
   Question.create!(
     title: Faker::Lorem.sentence(rand(1..4)),
     body: Faker::Hacker.say_something_smart,
-    user_id: rand(1..10)
+    user_id: rand(1..5)
     )
+end
+
+20.times do
+  Answer.create!(
+    body: Faker::Hipster.sentence + "?",
+    question_id: rand(1..10),
+    user_id: rand(1..5)
+  )
 end
