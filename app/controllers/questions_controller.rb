1cadcbf0a1c9643a35db9e4eede931d5e4c1af05
Julia Giraldi <email": null>
2016-04-20 20:33:55 -0400

    add questions index and show routes and erbs

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+
+get '/questions' do
+  @questions = Question.all
+  erb :'/questions/index'
+end
+
+get '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  erb :'/questions/show'
+end
+
