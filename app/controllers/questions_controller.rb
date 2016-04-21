0d016bd7ac6fc4418b264e6d8cf397d71675d70e
Julia Giraldi <email": null>
2016-04-21 11:17:39 -0400

    adds new question form and route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,8 +4,23 @@ get '/questions' do
   erb :'/questions/index'
 end
 
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
 get '/questions/:id' do
-  @question = Question.find_by(id: params[:id])
+  @question = Question.find_by(params[:id])
   erb :'/questions/show'
 end
 
+post '/questions' do
+  question = Question.new(
+    user_id: session[:user_id],
+    title: params[:title],
+    body: params[:body])
+  if question.save
+    redirect '/questions'
+  else
+    erb :'/questions/new'
+  end
+end
