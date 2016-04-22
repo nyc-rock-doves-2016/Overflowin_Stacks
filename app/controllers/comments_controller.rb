8979a7db61685f8381e5a4b7cd92117edffa91b2
Julia Giraldi <email": null>
2016-04-21 21:24:10 -0400

    adds working comments for questions and answers along with partial best answer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,26 @@
 get '/questions/:question_id/comments/new' do
-  question = Question.find_by(id: params[:question_id])
+  @question = Question.find_by(id: params[:question_id])
   if request.xhr?
-    erb :'/comments/_question_comment_partial', layout: false, locals: {question: question}
+    erb :'/comments/_question_comment_partial', layout: false, locals: {question: @question}
   else
-    erb :'/questions/show'
-    redirect '/questions/#{question.id}/comments/new'
-
-  end 
+    erb :'/comments/_question_comment_partial'
+  end
 end
 
 get '/answers/:answer_id/comments/new' do
-  answer = Answer.find_by(id: params[:answer_id])
+  @answer = Answer.find_by(id: params[:answer_id])
   if request.xhr?
-    erb :'/comments/_answer_comment_partial', layout: false, locals: {answer: answer}
+    erb :'/comments/_answer_comment_partial', layout: false, locals: {answer: @answer}
   else
   erb :'/comments/_answer_comment_partial'
-  redirect '/answers/#{answer.id}/comments/new'
+
   end
 end
 
 post '/questions/:id/comments' do
   comment = Comment.new(params[:comment])
   if comment.save
-    erb :'/questions/show'
+    redirect "/questions/#{comment.commentable_id}"
   else
     @errors = ["Missing title or body"]
     erb :'/comments/_question_comment_partial'
@@ -32,7 +30,7 @@ end
 post '/answers/:id/comments' do
   comment = Comment.new(params[:comment])
   if comment.save
-    erb :'/questions/show'
+    redirect "/questions/#{comment.commentable_id}"
   else
     @errors = ["Missing title or body"]
     erb :'/comments/_answer_comment_partial'
